d403d11902aa45454945979704c96423c60f4ffe
Jonathan Cammisuli <jon@cammisuli.ca>
2020-11-20 17:05:19 -0500

    update light color theme

---
+++ demo/rust.rs
@@ -0,0 +1,11 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
